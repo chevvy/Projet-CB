538d645072a774154c10da56261c0c0e4db8d2b5
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-05-05 19:49:50 -0400

    Add - Anim Jump avec controller

--- testPlatformer/Assets/Script/PlayerBehaviorV2.cs
+++ testPlatformer/Assets/Script/PlayerBehaviorV2.cs
@@ -5,65 +5,80 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehaviorV2 : MonoBehaviour
 {
-    public CharacterController playerCC;
-    public Animator playerAnimator;
+    private CharacterController m_PlayerCc;
+    private Animator m_PlayerAnimator;
+    
     public float horizontalSpeed = 1;
     public float fallForce = 1f;
     public float gravity = 2f;
     private static readonly int Horizontal = Animator.StringToHash("horizontal");
+    private static readonly int jumping = Animator.StringToHash("jumping");
 
     private Rigidbody Rigidbody => GetComponent<UnityEngine.Rigidbody>();
     private Vector3 m_MoveDirection;
 
-    private bool m_isMoving;
+    private bool m_IsMoving;
+    private bool m_IsJumping;
     // Start is called before the first frame update
     void Start()
     {
-        playerCC = GetComponent<CharacterController>();
-        playerAnimator = GetComponent<Animator>();
+        m_PlayerCc = GetComponent<CharacterController>();
+        m_PlayerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_isMoving)
+        if (m_PlayerCc.isGrounded && m_IsMoving)
         {
-            if (playerCC.isGrounded)
-            {
-                playerCC.Move(m_MoveDirection * Time.deltaTime);
-            }
+            MoveCharacter();
         }
-        if (!playerCC.isGrounded)
+      
+        if (!m_PlayerCc.isGrounded)
         {
-            Vector3 fall = new Vector3(0, -fallForce, 0);
-            playerCC.Move(fall);
+            CharacterFall();
         }
+    }
+
+    private void CharacterFall()
+    {
+        Vector3 fall = new Vector3(0, -fallForce, 0);
+        m_PlayerCc.Move(fall);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        // faut trouver un moyen d'utiliser le On HOLD du input manager pour permettre de 
-        // tenir le stick et de continuer à avancer 
         var joystickMovement = context.ReadValue<Vector2>();
-        playerAnimator.SetFloat(Horizontal, joystickMovement.x);
+        m_PlayerAnimator.SetFloat(Horizontal, joystickMovement.x);
         if (context.performed)
         {
             joystickMovement = context.ReadValue<Vector2>();
             m_MoveDirection = new Vector3(joystickMovement.x, 0, 0);
             m_MoveDirection *= horizontalSpeed;
-            m_isMoving = true;
+            m_IsMoving = true;
             //Debug.Log("Current movement = " + m_MoveDirection);
+        }
+        if (context.canceled)
+        {
+            m_IsMoving = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            m_PlayerAnimator.SetBool(jumping, true);
         }
 
         if (context.canceled)
         {
-            m_isMoving = false;
+            m_PlayerAnimator.SetBool(jumping, false);
         }
     }
-
-    private void MovePerformed()
+    private void MoveCharacter()
     {
-
+        m_PlayerCc.Move(m_MoveDirection * Time.deltaTime);
     }
 }
     
