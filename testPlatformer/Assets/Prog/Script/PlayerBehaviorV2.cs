b0361ed2f8b2aa3cf2bdc782a48165eff178d601
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-05-10 12:28:20 -0400

    Edit - Crosse moi le batonnet de fromage

--- testPlatformer/Assets/Prog/Script/PlayerBehaviorV2.cs
+++ testPlatformer/Assets/Prog/Script/PlayerBehaviorV2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerBehaviorV2 : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class PlayerBehaviorV2 : MonoBehaviour
     public float horizontalSpeed = 1;
     public float fallForce = 1f;
     public float gravity = 20f;
-    public float jumpSpeed = 8f;
+    [FormerlySerializedAs("jumpSpeed")] public float jumpForce = 8f;
     private static readonly int Horizontal = Animator.StringToHash("horizontal");
     private static readonly int jumping = Animator.StringToHash("jumping");
 
@@ -47,25 +48,7 @@ public class PlayerBehaviorV2 : MonoBehaviour
     private void MoveCharacter()
     {
         var testMove = _moveDirection * Time.deltaTime;
-        
-        if (_mPlayerCcIsGrounded && !_isJumping)
-        {
-            testMove.y = -0.1f;
-            _moveDirection.y = 0.1f;
-            m_PlayerCc.Move(testMove);
-            //Debug.Log("Moved whit modified= " + testMove);
-        }
-        else
-        {
-            if (testMove.y > jumpSpeed * Time.deltaTime)
-            {
-                Debug.Log("Test move was too much " + testMove.y);
-                testMove.y = jumpSpeed * Time.deltaTime;
-                _moveDirection.y = testMove.y;
-            }
-            m_PlayerCc.Move(testMove);    
-            //Debug.Log("Moved = " + testMove);
-        }
+        m_PlayerCc.Move(testMove);
         
     }
 
@@ -83,7 +66,7 @@ public class PlayerBehaviorV2 : MonoBehaviour
         {
             joystickMovement = context.ReadValue<Vector2>();
             _moveDirection.x = joystickMovement.x;
-            _moveDirection *= horizontalSpeed;
+            _moveDirection.x *= horizontalSpeed;
             //m_IsMoving = true;
             //Debug.Log("Current movement = " + m_MoveDirection);
         }
@@ -113,7 +96,7 @@ public class PlayerBehaviorV2 : MonoBehaviour
         if (m_PlayerCc.isGrounded && !_isJumping)
         {
             m_PlayerAnimator.SetBool(jumping, true);
-            _moveDirection.y = jumpSpeed;
+            _moveDirection.y = jumpForce;
             _moveDirection.y -= gravity * Time.deltaTime;
             //Debug.Log("Jumping : " + _moveDirection);
             _isJumping = true;
