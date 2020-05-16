860c7da1be8a9d707fd57722fc393d70c3d0a378
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-05-16 11:02:33 -0400

    Add - Basic attack + enemy death

---
+++ testPlatformer/Assets/PlayerWeapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public int dps = 5;
+    private Collider _weaponCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _weaponCollider = GetComponent<Collider>();
+    }
+    
+}
