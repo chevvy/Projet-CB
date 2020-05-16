a2c23fb4ebec31c1ed55aed12a8e30a80dc148e9
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-05-16 14:56:42 -0400

    Add - damage animation

--- testPlatformer/Assets/Enemy.cs
+++ testPlatformer/Assets/Enemy.cs
@@ -7,20 +7,28 @@ public class Enemy : MonoBehaviour
 {
     public int maxHealthPoint = 100;
     public bool isDead = false;
+    private bool _hasTakenDamage = false;
     
     private MeshRenderer _enemyMesh;
     private int _currentHealth;
+
+    public Material damageMaterial;
+    private Material _enemyMaterial;
     
-    
+
     void Start()
     {
         _enemyMesh = GetComponent<MeshRenderer>();
+        _enemyMaterial = _enemyMesh.material;
         _currentHealth = maxHealthPoint;
     }
 
     public void TakeDamage(int damage)
     {
+        _enemyMesh.material = damageMaterial;
         _currentHealth -= damage;
+        _hasTakenDamage = true;
+        StartCoroutine(ApplyDamageMaterial());
         CheckHealth();
     }
 
@@ -30,6 +38,7 @@ public class Enemy : MonoBehaviour
         {
             Die();    
         }
+        Debug.Log("debug");
     }
 
     private void Die()
@@ -37,5 +46,12 @@ public class Enemy : MonoBehaviour
         Debug.Log("enemy dieeed");
         GetComponent<Collider>().enabled = false;
         enabled = false;
+    }
+
+    IEnumerator ApplyDamageMaterial()
+    {
+        _enemyMesh.material = damageMaterial;
+        yield return new WaitForSeconds(.1f);
+        _enemyMesh.material = _enemyMaterial;
     }
 }
