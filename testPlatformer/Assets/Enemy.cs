858ec7b9fa73c233823644d755e8e118b8baf57c
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-05-16 11:02:33 -0400

    Add - Basic attack + enemy death

---
+++ testPlatformer/Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int healthPoint = 100;
+    public bool isDead = false;
+    private MeshRenderer _enemyMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyMesh = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (healthPoint <= 0)
+        {
+            _enemyMesh.enabled = false;
+            isDead = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerWeapon") && !isDead)
+        {
+            Debug.Log("ayoye");
+            healthPoint -= other.GetComponent<PlayerWeapon>().dps;
+        }
+    }
+}
