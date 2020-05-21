4a49cf3d36b146d0bc024f6953447de5f58840b3
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-05-21 17:38:31 -0400

    Edit - Fix audiomanager

--- testPlatformer/Assets/Prog/Script/AudioManager.cs
+++ testPlatformer/Assets/Prog/Script/AudioManager.cs
@@ -16,12 +16,12 @@ public class AudioManager : MonoBehaviour
         {
             //Check if there is already an instance of SoundManager
             if (instance == null)
-                //if not, set it to this.
+            {
                 instance = this;
-            //If instance already exists:
-            else if (instance != this)
-                //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+            } else if (instance != this)
+            {
                 Destroy (gameObject);
+            }
 
             //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
             DontDestroyOnLoad (gameObject);
