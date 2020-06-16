d927df6f99c80389688dc524b1f38034f47a9985
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-06-16 17:19:30 -0400

    Fix: Weapon impact / attack

---
+++ testPlatformer/Assets/Prog/Script/WeaponImpactDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Prog.Script;
+using UnityEngine;
+
+public class WeaponImpactDetector : MonoBehaviour
+{
+    public PlayerCombat playerCombat;
+    
+    // TODO Mettre des vérifications pour être certains d'avoir un rigidbody et un collider sur le mesh
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Enemy"))
+        {
+            playerCombat.Attack();
+        }
+    }
+}
