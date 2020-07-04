9cd37847100dd221bc3abeb94d25315f7503d760
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-07-04 12:37:49 -0400

    Fix: Combat fonctionne bieeen

--- testPlatformer/Assets/Prog/Script/WeaponImpactDetector.cs
+++ testPlatformer/Assets/Prog/Script/WeaponImpactDetector.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Prog.Script;
 using UnityEngine;
@@ -8,11 +9,19 @@ public class WeaponImpactDetector : MonoBehaviour
     public PlayerCombat playerCombat;
     
     // TODO Mettre des vérifications pour être certains d'avoir un rigidbody et un collider sur le mesh
-    void OnCollisionEnter(Collision other)
+    // void OnCollisionEnter(Collision other)
+    // {
+    //     if (other.collider.CompareTag("Enemy"))
+    //     {
+    //         playerCombat.Attack();
+    //     }
+    // }
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.collider.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy"))
         {
-            playerCombat.Attack();
+            playerCombat.Attack(other.GetComponent<Enemy>());
         }
     }
 }
