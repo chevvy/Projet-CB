41bc40d9a5848c42a12c2d72559699f507ce694a
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2020-07-18 14:41:20 -0400

    Add - Armor piece script, ApplyForce static scripts (and refactor)
    Avec la nouvelle méthode static ApplyForce.OnAttack(), on va pouvoir appliquer des impacts physiques un peu partout sans recopier du code en cochon

---
+++ testPlatformer/Assets/Prog/Script/Armor/ArmorPiece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NSubstitute.ExceptionExtensions;
+using Prog.Script.RigidbodyInteraction;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class ArmorPiece : MonoBehaviour
+{
+    public bool isArmorPieceInTheBack = false;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        if (!_rigidbody.isKinematic)
+        {
+            Debug.LogError("The armorPiece '" + name + "' rigidBody must be kinematic");
+        }
+    }
+
+    /// <summary>
+    /// On va venir débarquer la pièce d'armure en désactivant "kinematic"
+    /// et en lui appliquant de la force en fonction de la position du joueur
+    /// </summary>
+    /// <param name="xAttackOriginPosition">Position en x du player</param>
+    public void RemoveArmorPiece(float xAttackOriginPosition)
+    {
+        _rigidbody.isKinematic = false;
+        ApplyForce.OnAttack(_rigidbody, null, xAttackOriginPosition, 5, 5);
+    }
+}
