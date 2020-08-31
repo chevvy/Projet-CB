f8798030edc694687f7211fbb4fbfe62f36e85a0
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-08-30 20:00:16 -0400

    Add - functionnal patrol behavior on robot

--- CB/Assets/Prog/Script/AI/MoveTowardTarget.cs
+++ CB/Assets/Prog/Script/AI/MoveTowardTarget.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Prog.Script.RigidbodyInteraction;
+using UnityEngine;
 using UnityEngine.AI;
 
 namespace Prog.Script.AI
@@ -7,29 +8,35 @@ namespace Prog.Script.AI
     {
         private readonly BasicRobotBehavior _robot;
         private readonly NavMeshAgent _navMeshAgent;
+
         private readonly Animator _animator;
         private static readonly int Speed = Animator.StringToHash("Speed");
         private static readonly int MoveLeft = Animator.StringToHash("Walk_L");
         private static readonly int MoveRight = Animator.StringToHash("Walk_R");
+        
+        private CheckDirection _direction = new CheckDirection();
+        private Transform _oldTranform;
 
         public MoveTowardTarget(BasicRobotBehavior robot, NavMeshAgent navMeshAgent, Animator animator)
         {
             _robot = robot;
             _navMeshAgent = navMeshAgent;
             _animator = animator;
+            _oldTranform = _robot.transform;
         }
 
 
         public void Tick()
         {
-            // Move vers l'enemi
-            // animator move
+            
         }
 
         public void OnEnter()
         {
             _navMeshAgent.enabled = true;
             _navMeshAgent.SetDestination(_robot.Target.transform.position);
+            _animator.SetBool(_direction.IsGoingLeft(_robot.transform, _robot.Target.transform) ? 
+                    MoveLeft : MoveRight,true);
         }
 
         public void OnExit()
