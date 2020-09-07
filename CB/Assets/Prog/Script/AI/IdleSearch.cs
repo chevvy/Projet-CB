bd547c18cbd7b6f52b63a4ad046c37cf9ff1d706
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-09-07 12:01:12 -0400

    Add - Idle Search when reaching target

---
+++ CB/Assets/Prog/Script/AI/IdleSearch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using Prog.Script.RigidbodyInteraction;
+using UnityEngine;
+
+namespace Prog.Script.AI
+{
+    public class IdleSearch : IState
+    {
+        private readonly Animator _animator;
+        private static readonly int IdleLeft = Animator.StringToHash("Idle_L");
+        private static readonly int IdleRight = Animator.StringToHash("Idle_R");
+
+        private readonly BasicRobotBehavior _robot;
+        private readonly CheckDirection _direction = new CheckDirection();
+
+        public IdleSearch( BasicRobotBehavior robot, Animator animator)
+        {
+            _animator = animator;
+            _robot = robot;
+        }
+        public void Tick() {}
+
+        public void OnEnter()
+        {
+            _robot.IsSearching = true;
+            _robot.StartCoroutine(SearchForTarget());
+        }
+
+        IEnumerator SearchForTarget()
+        {
+            SetAnimationRelativeToDirection();
+            yield return new WaitForSeconds(2);
+            _robot.IsSearching = false;
+        }
+
+        private void SetAnimationRelativeToDirection()
+        {
+            _animator.SetBool(
+                _direction.IsGoingLeft(_robot.transform, _robot.Target.transform) ? 
+                    IdleLeft : IdleRight, true
+            );
+        }
+
+        public void OnExit()
+        {
+            _robot.IsSearching = false;
+        }
+    }
+}
