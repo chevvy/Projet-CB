f5a23be21f50fbf4ad0e08e667d8f29e8fd623b1
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-09-20 19:16:48 -0400

    Add - React to animation/physics when attacked

--- CB/Assets/Prog/Script/AI/BasicRobotBehavior.cs
+++ CB/Assets/Prog/Script/AI/BasicRobotBehavior.cs
@@ -10,7 +10,12 @@ public class BasicRobotBehavior : MonoBehaviour
     
     public Target Target { get; set; }
     public Target[] patrolPoints;
-    public bool IsSearching = false;
+    public LayerMask groundLayerMask;
+    public bool isSearching = false;
+    public bool isGrounded;
+    public bool isGettingAttacked;
+    public float groundCheckerRadius = 1f;
+    public float playerPosition;
     private void Awake()
     {
         var navMeshAgent = GetComponent<NavMeshAgent>();
@@ -24,26 +29,28 @@ public class BasicRobotBehavior : MonoBehaviour
         var searchForTarget = new SearchForTarget(this); // State qui cherche le player avec un raycast
         var moveTowardTarget = new MoveTowardTarget(this, navMeshAgent, animator);
         var idleSearch = new IdleSearch(this, animator);
-        
-        //var alerted = new AlertedRobot() // State qui vient de détecter le player
-        //var moving = new MovingTowardEnemy() // State lorsque l'enemy va vers le player
-        //var attacking = new RobotAttacking() // State lorsque le robot attaque le player
-        //var attacked = new  RobotGettingAttacked() // State lorsque le robot recoit une attaque
+        var takesDamage = new TakesDamage(this, animator, navMeshAgent);
 
+        AddTransition(idleSearch, searchForTarget, HasFinishedSearching());
         AddTransition(searchForTarget, moveTowardTarget, HasTarget());
         AddTransition(moveTowardTarget, idleSearch, HasReachedDestination());
-        AddTransition(idleSearch, searchForTarget, HasFinishedSearching());
-        // AddTransition(moveTowardTarget, searchForTarget, HasReachedDestination());
-        // add anyTransitions pour des escape state
-        
+
+        AddTransition(takesDamage, searchForTarget, HasLanded());
+        AddAnyTransition(takesDamage, HasTakenDamage());
+
         _stateMachine.SetState(searchForTarget);
         
         void AddTransition(IState to, IState from, Func<bool> conditon) =>
             _stateMachine.AddTransition(to, from, conditon);
 
+        void AddAnyTransition(IState to, Func<bool> condition) =>
+            _stateMachine.AddAnyTransition(to, condition);
+
         Func<bool> HasTarget() => () => Target != null;
         Func<bool> HasReachedDestination() => () => !navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f;
-        Func<bool> HasFinishedSearching() => () => !IsSearching;
+        Func<bool> HasFinishedSearching() => () => !isSearching;
+        Func<bool> HasTakenDamage() => () => isGettingAttacked;
+        Func<bool> HasLanded() => () => isGrounded;
     }
     
     private void Update() => _stateMachine.Tick();
