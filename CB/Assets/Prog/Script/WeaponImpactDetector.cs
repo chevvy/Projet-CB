9b0e62260eeca5d3a81b2485b0236c663cc8b6bc
Vincent Chavanel Jobin <vincent.chavanel-jobin.1@ulaval.ca>
2020-10-26 11:18:33 -0400

    Fix - Major rework attack system ! Event based now
    Ça fait calisseeement le café

--- CB/Assets/Prog/Script/WeaponImpactDetector.cs
+++ CB/Assets/Prog/Script/WeaponImpactDetector.cs
@@ -1,20 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Prog.Script;
+﻿using Prog.Script;
 using UnityEngine;
 
 public class WeaponImpactDetector : MonoBehaviour
 {
     public PlayerCombat playerCombat;
-    
+
     // TODO Mettre des vérifications pour être certains d'avoir un rigidbody et un collider sur le mesh
 
-    private void OnTriggerEnter(Collider other)
+    public void CheckForEnemies() // TODO refactor pour une méthode qui return la liste d'ennemy et autre méthode qui envoi l'attaque pour être plus testable
     {
-        if (other.CompareTag("Enemy"))
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2f);
+        foreach (var hitCollider in hitColliders)
         {
-            playerCombat.Attack(other.GetComponent<Enemy>());
+            if (!hitCollider.CompareTag("Enemy")) continue;
+            playerCombat.Attack(hitCollider.GetComponent<Enemy>());
         }
     }
 }
